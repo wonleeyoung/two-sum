5abf57caeaf8f78c46ce66775d0117098ab38b18
wonyounglee <lwy970327@naver.com>
2023-03-09 19:39:07 +0900

    submit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-
-def twoSum(nums: List[int], target: int) -> List[int]:
-	return 
+def twoSum(nums: list[int], target: int) -> list[int]:
+  a = []
+  for i in range(len(nums)-1):
+    for j in range(i+1, len(nums)):
+      if nums[i] + nums[j] == target:
+        a.append(i)
+        a.append(j)
+        break
+  return a
